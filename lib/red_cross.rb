c2409a68fe793064f8cdf1e3b73c403c19e53c82
ronbarab <rbarabash@yotpo.com>
2015-12-15 18:49:35 +0200

    Added flush for async operations

--- lib/red_cross.rb
+++ lib/red_cross.rb
@@ -7,10 +7,12 @@ module RedCross
   class << self
     def track(attrs, topic = "")
       Configuration.segment.track(attrs)
+      Configuration.segment.flush
     end
 
     def identify(attrs, topic = "")
       Configuration.segment.identify(attrs)
+      Configuration.segment.flush
     end
   end
 end
