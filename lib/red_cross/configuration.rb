9fe9e519d29a619777e557d238f8a2162af047bc
Gitelzon Dmitri <dmitri@yotpo.com>
2016-07-20 13:27:22 +0300

    Added influxdb integration

--- lib/red_cross/configuration.rb
+++ lib/red_cross/configuration.rb
@@ -1,17 +1,32 @@
 module RedCross
   module Configuration
     class << self
-      attr_accessor :path, :segment_write_key, :segment
+      attr_accessor :path, :segment_write_key, :segment, :influxdb_creds, :influxdb
 
       def configure
         yield self
         init_segment
+        init_influxdb
       end
 
       private
+
       def init_segment
-        @segment = Segment::Analytics.new({write_key: segment_write_key})
+        @segment = Segment::Analytics.new({write_key: (ENV['SEGMENT_WRITE_KEY'] || segment_write_key)})
       end
+
+      def init_influxdb
+        @influxdb = InfluxDB::Rails.configure do |config|
+          config.influxdb_database = "yotpo_api_" + Rails.env
+          config.influxdb_hosts    = [ENV['INFLUXDB_HOST']]
+          config.influxdb_port     = ENV['INFLUXDB_PORT']
+
+          config.series_name_for_controller_runtimes = "controller"
+          config.series_name_for_view_runtimes       = "view"
+          config.series_name_for_db_runtimes         = "db"
+        end
+      end
+
     end
   end
 end
