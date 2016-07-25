4452a5e5a73ab64d78db050c26907c4d4d95c58b
Gitelzon Dmitri <dmitri@yotpo.com>
2016-07-25 12:59:20 +0300

    Added default params to integrations

--- lib/red_cross/configuration.rb
+++ lib/red_cross/configuration.rb
@@ -1,7 +1,7 @@
 module RedCross
   module Configuration
     class << self
-      attr_accessor :path, :segment_write_key, :segment, :influxdb_creds, :influxdb
+      attr_accessor :path, :segment_write_key, :segment, :influxdb_connect, :influxdb
 
       def configure
         yield self
@@ -12,19 +12,19 @@ module RedCross
       private
 
       def init_segment
-        @segment = Segment::Analytics.new({write_key: (ENV['SEGMENT_WRITE_KEY'] || segment_write_key)})
+        @segment = Segment::Analytics.new({write_key:  segment_write_key})
       end
 
       def init_influxdb
-        @influxdb = InfluxDB::Rails.configure do |config|
-          config.influxdb_database = "yotpo_api_" + Rails.env
-          config.influxdb_hosts    = [ENV['INFLUXDB_HOST']]
-          config.influxdb_port     = ENV['INFLUXDB_PORT']
-
+        InfluxDB::Rails.configure do |config|
+          config.influxdb_database = influxdb_connect[:database] || 'red_cross_db'
+          config.influxdb_hosts    = [(influxdb_connect[:host] || 'localhost')]
+          config.influxdb_port     = influxdb_connect[:port] || '8086'
           config.series_name_for_controller_runtimes = "controller"
           config.series_name_for_view_runtimes       = "view"
           config.series_name_for_db_runtimes         = "db"
         end
+        @influxdb = InfluxDB::Rails.client
       end
 
     end
