cf9b3841a2f9f184c5c1279a17c144def96e6744
Gitelzon Dmitri <dmitri@yotpo.com>
2017-02-07 11:11:50 +0200

    feat(Resque::Plugoins::RedCrossAsync):job run time

--- lib/resque/plugins/red_cross_async.rb
+++ lib/resque/plugins/red_cross_async.rb
@@ -7,23 +7,29 @@ module Resque
         {}
       end
 
-      def after_perform_actions(*args)
-        RedCross.flush
-        send_metrics('performed', *args)
-      end
-
       def before_perform_send_monitor_data(*args)
+        @job_start_time = Time.now().to_f
+        @job_end_time = @job_start_time
         send_metrics('before_perform', *args)
       end
 
+      def after_perform_actions(*args)
+        RedCross.flush
+        @job_end_time = Time.now().to_f
+        send_metrics('performed', *args)
+      end
+
       def on_failure_send_monitor_data(e, *args)
+        @job_end_time = Time.now().to_f
         send_metrics('failed', *args)
       end
 
       private
 
       def send_metrics(event_type, *args)
-        RedCross.monitor_track(event: 'resque_job_' + event_type, properties: { class: self.name }.merge(properties(*args)))
+        properties = { class: self.name }.merge(properties(*args))
+        properties.merge!({ fields: { run_time: ((@job_end_time - @job_start_time)*1000).to_i } }) unless event_type == 'before_perform'
+        RedCross.monitor_track(event: 'resque_job_' + event_type, properties: properties)
       end
     end
   end
