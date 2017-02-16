2fb71eee298d8639d5e2f28bd635aeb79e948756
dmitri86git <dmitri86git@users.noreply.github.com>
2017-02-16 14:45:37 +0200

    Mbia 60 scheduler monitor (#11)
    * feat(Resque::Scheduler::DelayingExtensions):send event before task enque
    * feat(RedCrossMonitor):track resque hooks and job run time

---
+++ lib/red_cross/resque/scheduler.rb
@@ -0,0 +1,15 @@
+module Resque
+  module Scheduler
+    module DelayingExtensions
+      alias default_last_enqueued_at last_enqueued_at
+      def last_enqueued_at(name, timestemp)
+        properties = {
+            task_name: name,
+            status: 'enqueued'
+        }
+        RedCross.monitor_track(event: 'resque_scheduler', properties: properties)
+        default_last_enqueued_at(name, timestemp)
+      end
+    end
+  end
+end
