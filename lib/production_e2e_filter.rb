c4930a3c4c2e8757e19836e1eaef8842150d7522
Ron Hasson <rhasson@yotpo.com>
2022-10-19 18:05:00 -0700

    feat(production_e2e_filter): refactored to fix multiple errors on module load

--- lib/production_e2e_filter.rb
+++ lib/production_e2e_filter.rb
@@ -1,61 +1,65 @@
-require '../lib/red_cross/logging'
+require 'yaml'
+require 'erb'
+require_relative 'red_cross/logging'
 
-BEGIN {
-  begin
-    E2E_FILTER_CONF = YAML::load(ERB.new(IO.read(File.expand_path('../conf/e2e_filter_conf.yml', __FILE__))).result, fallback: []).with_indifferent_access
-    RedCross::Logging.log(Logger::INFO, {
-                  log_tag: 'production_e2e_filter',
-                  message: "Successfully initialized red_cross production e2e filter values"
-                })
-  rescue => e
-    RedCross::Logging.log(Logger::Error, {
-                   log_tag: 'production_e2e_filter',
-                   message: "Unable to initialize red_cross production e2e filter due to #{e.message}"
-                 })
-  end
-}
+include RedCross::Logging
 
-module ProductionE2EFilter
-  @e2e_emails = E2E_FILTER_CONF['PREFIXES']['EMAILS']
-  @e2e_domains = E2E_FILTER_CONF['PREFIXES']['DOMAINS']
+module RedCross
+  module ProductionE2EFilter
 
-  def self.log_tag
-    return 'production_e2e_filter'
-  end
+    def self.log_tag
+      'production_e2e_filter'
+    end
 
-  def is_e2e_test_flow?(properties)
     begin
-      return false unless is_filtering_enabled?
-      filter_email?(properties[:email]) || filter_domain?(properties[:website], properties[:storeDomain]) ? true : false
+      @e2e_filter_conf = YAML::load(ERB.new(IO.read(File.expand_path('../../conf/e2e_filter_conf.yml', __FILE__))).result)
+      @e2e_emails = @e2e_filter_conf['PREFIXES']['EMAILS']
+      @e2e_domains = @e2e_filter_conf['PREFIXES']['DOMAINS']
+      RedCross::Logging.log(Logger::INFO, {
+        log_tag: self.log_tag,
+        message: "Successfully initialized red_cross production e2e filter values"
+      })
     rescue => e
       RedCross::Logging.log(Logger::Error, {
-                     log_tag: self.log_tag,
-                     message: "Failed to check for production e2e filter values due to #{e.message}"
-                   })
-      false
+        log_tag: self.log_tag,
+        message: "Unable to initialize red_cross production e2e filter due to #{e.message}"
+      })
     end
-  end
 
-  private
-
-  def is_filtering_enabled?
-    E2E_FILTER_CONF['EVENT_FILTERING_ENABLED']
-  end
-
-  def filter_email?(email)
-    @e2e_emails.each do |test_prefix|
-      return true if email&.start_with?(test_prefix)
-    end
-    false
-  end
-
-  def filter_domain?(*domain_candidates)
-    @e2e_domains.each do |test_prefix|
-      domain_candidates.each do |domain_candidate|
-        return true if domain_candidate.start_with?(test_prefix)
+    def is_e2e_test_flow?(properties)
+      begin
+        return false unless is_filtering_enabled?
+        filter_email?(properties[:email]) || filter_domain?(properties[:website], properties[:storeDomain]) ? true : false
+      rescue => e
+        RedCross::Logging.log(Logger::Error, {
+          log_tag: self.log_tag,
+          message: "Failed to check for production e2e filter values due to #{e.message}"
+        })
+        false
       end
     end
-    false
-  end
 
+    private
+
+    def is_filtering_enabled?
+      @e2e_filter_conf['EVENT_FILTERING_ENABLED']
+    end
+
+    def filter_email?(email)
+      @e2e_emails.each do |test_prefix|
+        return true if email&.start_with?(test_prefix)
+      end
+      false
+    end
+
+    def filter_domain?(*domain_candidates)
+      @e2e_domains.each do |test_prefix|
+        domain_candidates.each do |domain_candidate|
+          return true if domain_candidate.start_with?(test_prefix)
+        end
+      end
+      false
+    end
+
+  end
 end
